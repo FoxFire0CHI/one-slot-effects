1eafbe245d15827831a52b55c9a8ef7c389cd959
FoxFire0 <143306770+FoxFire0CHI@users.noreply.github.com>
2026-08-14 12:07:26 -0500

    Attempt to fix memory issue

--- src/lib.rs
+++ src/lib.rs
@@ -540,11 +540,51 @@ unsafe fn get_raw_eff_data(ctx: &mut skyline::hooks::InlineCtx) {
 #[skyline::from_offset(0x3ac560)]
 unsafe fn battle_object_from_id(id: u32) -> *mut u32;
 
+#[skyline::from_offset(0x3562cf0)]
+pub fn unload_effects(manager: *mut u64, handle: u32);
+
+#[skyline::from_offset(0x355eec0)]
+pub fn load_effects(manager: *mut u64, handle: u32, search_index: &u32) -> u32;
+
 // this hook was previously ran on 0x60bfd8. it is moved up 0x4 so that it runs before that same offset used in smashline
 // this in turn allows smashline's transplanted effects to still work without affecting one slot effects
 // otherwise, if they share the offset, OSE cannot properly cache effects beyond the first file loaded for any given fighter
 #[skyline::hook(offset = 0x60bfd4, inline)]
 unsafe fn tmp(ctx: &mut skyline::hooks::InlineCtx) {
+    let search_index_begin = &*(*ctx.registers[2].x.as_ref() as *const u32);
+    let result = load_effects(
+        *ctx.registers[0].x.as_ref() as _,
+        *ctx.registers[1].x.as_ref() as u32,
+        search_index_begin,
+    );
+    let info = resources::types::FilesystemInfo::instance().unwrap();
+    let search = info.search();
+    let path_name = search.get_path_list()[*search_index_begin as usize];
+    let full_name = Hash40(path_name.path.hash40().0).concat_str("/transplant");
+    let Ok(first_child) = search.get_first_child_in_folder(smash_arc::Hash40(full_name.0)) else {
+        println!("Nothing to transplant");
+        return
+    };
+    let mut current_child = first_child;
+    let mut num_transplants = 0;
+    loop {
+        let Ok(path_index) = search.get_path_index_from_hash(smash_arc::Hash40(current_child.path.hash40().0)) else {
+            break;
+        };
+        println!("Found one!");
+        let index = path_index.index();
+        num_transplants += 1;
+        let result = load_effects(
+            *ctx.registers[0].x.as_ref() as _,
+            *ctx.registers[1].x.as_ref() as u32 + num_transplants,
+            &index,
+        );
+        println!("result: {result}");
+        let Ok(next_child) = search.get_next_child_in_folder(current_child) else {
+            break;
+        };
+        current_child = next_child;
+    }
     let Some(slot) = EFF_FIGHTER_SLOT.as_ref() else { return };
     let slot = 1 + *slot as u32;
     *ctx.registers[1].w.as_mut() += slot * 0x1000;
